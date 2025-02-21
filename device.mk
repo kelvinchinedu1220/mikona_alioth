5b2d9c7421b54e1c9a33c5059e46371148941ccd
CHINEDU UTTAH <kelvinnino07@gmail.com>
2025-02-21 22:42:22 +0000

    Edit device.mk

--- device.mk
+++ device.mk
@@ -114,9 +114,3 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
-
-# OEM otacert
-PRODUCT_EXTRA_RECOVERY_KEYS += \
-    vendor/recovery/security/miui
-
-
